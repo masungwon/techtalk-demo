74afb7b85c7a25312e81574ae77a78d22f5ad67b
masungwon <sungwon.ma.16@gmail.com>
2016-09-25 10:43:32 -0400

    Update load-balancer.js

--- load-balancer.js
+++ load-balancer.js
@@ -1,12 +1,35 @@
-// launch with: 'node load-balancer.js 8000' on terminal
+/*
+* This is a simple round-robin load balancer
+* Launch with: 'node load-balancer.js 8000' on terminal
+*/
 let PORT = process.argv.splice(2)[0];  // 8000
 let httpProxy = require('http-proxy');
 
-// Ports to use in the round robin proxy
-let ports = [8001, 8002, 8003];
+// List of servers we want to use in the rotation
+let addresses = [
+    {
+        host: 'localhost',
+        port: 8001
+    },
+    {
+        host: 'localhost',
+        port: 8002
+    },
+    {
+        host: 'localhost',
+        port: 8003
+    }
+];
 
-let i = 0;
 httpProxy.createServer(function (req, res, proxy) {
-    proxy.web(req, res, { target: 'http://localhost:' + ports[i] + '/'});
-    i = (i + 1) % ports.length;
+    // on each request, get the first location from the addresse list
+    let target = { target: addresses.shift() };
+
+    // proxy to the server whose 'turn' it is
+    console.log('balancing request to: ', target);
+    proxy.web(req, res, target);
+
+    // and then the server you just used becomes the last item on the list
+    addresses.push(target.target);
+
 }).listen(PORT || 8000);
